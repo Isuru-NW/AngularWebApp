f20f9eb3bcbe9b366c951eb1f6b75c391be58e23
unknown <isuru.2018530@iit.ac.lk>
2022-12-19 19:33:54 +0530

    Full code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '../location';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,46 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  city = '';
+  searchResults: Location[] = [];
+  housingList: Location[] = [
+    {
+      id: 1,
+      name: 'Location One',
+      desccription: 'Safe place',
+      city: 'Chicago'
+    },
+    {
+      id: 2,
+      name: 'Location Two',
+      desccription: 'Safe place',
+      city: 'Torando'
+    },
+    {
+      id: 1,
+      name: 'Location Three',
+      desccription: 'Safe place',
+      city: 'Benington'
+    },
+    {
+      id: 1,
+      name: 'Location Four',
+      desccription: 'Safe place',
+      city: 'Aukshire'
+    }
+  ];
+
   ngOnInit(): void {
+  }
+
+  searchHousing() {
+    if (this.city !== '') {
+      this.searchResults = this.housingList
+                          .filter(location => 
+                            location.city.toLowerCase() === 
+                            this.city.toLocaleLowerCase());
+      this.searchResults;
+    }
   }
 
 }
